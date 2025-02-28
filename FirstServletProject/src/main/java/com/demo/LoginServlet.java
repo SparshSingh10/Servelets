fd3e2352fc10fc733fb38befa5fbbc58390b2063
SparshSingh10 <sparsh262002@gmail.com>
2025-02-28 23:47:35 +0530

    uc4 28 feb

--- FirstServletProject/src/main/java/com/demo/LoginServlet.java
+++ FirstServletProject/src/main/java/com/demo/LoginServlet.java
@@ -16,8 +16,8 @@ import java.util.regex.Pattern;
         description = "Login Servlet Testing",
         urlPatterns = { "/LoginServlet" },
         initParams = {
-                @WebInitParam(name = "user", value = "arunodaya"),
-                @WebInitParam(name = "password", value = "jivi@11AJ"),
+                @WebInitParam(name = "user", value = "sparsh"),
+                @WebInitParam(name = "password", value = "Passw0rdd"),
         }
 )
 
@@ -27,24 +27,35 @@ public class LoginServlet extends HttpServlet {
         String user = request.getParameter("user");
         String pwd = request.getParameter("pwd");
 
-        String regex = "^[A-Z][A-Za-z]{2,}$";
-        Pattern namePattern = Pattern.compile(regex);
-        Matcher nameMatcher = namePattern.matcher(user);
+        // Username validation: Must start with a capital letter and be at least 3 characters long
+        String usernameRegex = "^[A-Z][A-Za-z]{2,}$";
+        Pattern usernamePattern = Pattern.compile(usernameRegex);
+        Matcher usernameMatcher = usernamePattern.matcher(user);
+
+        // Updated password validation regex
+        String passwordRegex = "^(?=.*[A-Z])(?=.*\\d)(?=[^!@#$%^&*()_+={}\\[\\]:;\"'<>,.?/]*[!@#$%^&*()_+={}\\[\\]:;\"'<>,.?/][^!@#$%^&*()_+={}\\[\\]:;\"'<>,.?/]*$).{8,}$";
+        Pattern passwordPattern = Pattern.compile(passwordRegex);
+        Matcher passwordMatcher = passwordPattern.matcher(pwd);
+
 
         String userId = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if(!nameMatcher.matches()) {
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-            PrintWriter out = response.getWriter();
-            out.println("<font color=red>Username must starts with Caps and has atleast 3 characters long</font");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+
+        if (!usernameMatcher.matches()) {
+            out.println("<font color=red>Username must start with a capital letter and have at least 3 characters</font>");
             rd.include(request, response);
-        } else if(userId.equals(user) && password.equals(pwd)) {
+        } else if (!passwordMatcher.matches()) {
+            out.println("<font color=red>Password must be at least 8 characters long, have at least 1 uppercase letter, 1 number, and exactly 1 special character</font>");
+            rd.include(request, response);
+        } else if (userId.equals(user) && password.equals(pwd)) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-            PrintWriter out = response.getWriter();
-            out.println("<font color=red>Either username or password is wrong</font");
+            out.println("<font color=red>Either username or password is incorrect</font>");
             rd.include(request, response);
         }
     }
