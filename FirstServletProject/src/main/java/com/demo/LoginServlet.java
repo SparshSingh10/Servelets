ec54bc4ee362cab15a8b1d2ec3e9cf4bb9b2defd
SparshSingh10 <sparsh262002@gmail.com>
2025-02-28 17:20:32 +0530

    uc3 28 feb

--- FirstServletProject/src/main/java/com/demo/LoginServlet.java
+++ FirstServletProject/src/main/java/com/demo/LoginServlet.java
@@ -9,13 +9,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
         urlPatterns = { "/LoginServlet" },
         initParams = {
-                @WebInitParam(name = "user", value = "Narayan"),
-                @WebInitParam(name = "password", value = "AbraKaDabra"),
+                @WebInitParam(name = "user", value = "arunodaya"),
+                @WebInitParam(name = "password", value = "jivi@11AJ"),
         }
 )
 
@@ -25,9 +27,18 @@ public class LoginServlet extends HttpServlet {
         String user = request.getParameter("user");
         String pwd = request.getParameter("pwd");
 
+        String regex = "^[A-Z][A-Za-z]{2,}$";
+        Pattern namePattern = Pattern.compile(regex);
+        Matcher nameMatcher = namePattern.matcher(user);
+
         String userId = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if(userId.equals(user) && password.equals(pwd)) {
+        if(!nameMatcher.matches()) {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Username must starts with Caps and has atleast 3 characters long</font");
+            rd.include(request, response);
+        } else if(userId.equals(user) && password.equals(pwd)) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
